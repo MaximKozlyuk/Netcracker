1e3d9bdae4570d22c262b3879cad968beade0e82
MaximKozlyuk <maxim.kozlyuk@yandex.ru>
2019-02-26 00:29:16 +0400

    js

--- Semestr2/src/main/frontend/script.js
+++ Semestr2/src/main/frontend/script.js
@@ -1,5 +1,41 @@
 
 
-function onHover () {
+function onMouseEnterId () {
+    document.getElementById("emp-id-input").value = "Example : 7432";
+}
 
+function onMouseOutId () {
+    document.getElementById("emp-id-input").value = "";
+}
+
+function onMouseEnterName() {
+    document.getElementById("emp-name-input").value = "Ivan Ivanovich";
+}
+
+function onMouseOutName () {
+    document.getElementById("emp-name-input").value = "";
+}
+
+function validateName () {
+    var input = document.getElementById("emp-name-input");
+    var matches = input.value.match(/\d+/g);
+    if (matches != null) {
+        alert("Name should not contain digits");
+        input.value = "";
+        return;
+    }
+    transfer();
+}
+
+function transfer() {
+    //var a = document.f.elements['name'].value;
+    var name = document.getElementById('emp-name-input').value;
+    window.location.href = "newEmployee.html?" + name;
+}
+
+function confirmDelete (itemToDelete) {
+    var answer = confirm("R u sure 2 delete dat stuff?");
+    if (answer) {
+        document.getElementById(itemToDelete).remove();
+    }
 }
